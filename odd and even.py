793f7a37f460b0abf2ac1384680cbc0a8506f31c
thesmitpatel <61879629+thesmitpatel@users.noreply.github.com>
2021-10-01 20:04:07 +0530

    Create odd and even.py

---
+++ odd and even.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+num = int(input("enter any number"))
+if num%2==0:
+    print(num," is even")
+else:
+    print(num," is odd")
+
+
+# In[ ]:
+
+
+
+
